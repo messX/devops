8e55d3034a164ac55ed0d267d98b95ae0904c47c
messx <dhandedhan@gmail.com>
2019-03-23 18:27:43 +0530

    fixes

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@ git fetch --all
 git clean -f
 git checkout master
 git pull origin master
-sudo docker build -t metrippingnginxexporter .
 sudo docker rm -f metrippingnginxexporter
 sh docker-cmd.sh
+DOCKER_REGISTRY='eu.gcr.io/metripping-149707'
+SERVICE='mt-nginx-monitor'
+RELEASE_VERSION='0.1'
+RELEASE_TAG=`date`
+PROJECT_ID="metripping-149707"
+image_name="mt-nginx-monitor"
+image_tag="${RELEASE_TAG}:${RELEASE_VERSION}"
