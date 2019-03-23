3872a42f952bbd1f8b64d067aece46e845bd3abb
messx <dhandedhan@gmail.com>
2019-03-23 15:55:54 +0530

    exporter

--- collector.py
+++ collector.py
@@ -4,7 +4,7 @@ from generator import Generator
 
 
 def run_server_watch_file():
-    _file = '/mnt/logs/access.log'
+    _file = '/home/ubuntu/devops/access.log'
     # Start up the server to expose the metrics.
     start_http_server(8080)
     Generator(_file).watch_file()
