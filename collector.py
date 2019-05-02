a280e55e0d1494c5c30903032eba175e5363ae61
rptgautam <gautam.official.it@gmail.com>
2019-05-02 12:28:42 +0530

    fixes

--- collector.py
+++ collector.py
@@ -8,10 +8,7 @@ def run_server_watch_file():
     """
     Start up the server to expose the metrics.
     """
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    log_dir = '{}'.format(dir_path)
-    files = [file for file in os.listdir(log_dir) if file.endswith('.log')]
-    _file = '{}/{}'.format(log_dir, files[0])
+    _file = '/mnt/logs/access.log'
     start_http_server(8080)
     print('server started at  localhost:8080')
     Generator(_file).watch_file()
